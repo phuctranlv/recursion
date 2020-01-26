4e37b3be650340a91756a85c61e8c681e32d6c01
Phuc Tran <phuctranlv@gmail.com>
2020-01-25 21:11:39 -0800

    Complete stringifyJSON function definition

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'number') {
+    return obj.toString();
+  } else if (typeof obj === 'boolean') {
+    return obj.toString();
+  } else if (obj === null) {
+    return 'null';
+  } else if (Array.isArray(obj) && obj.length === 0) {
+    return '[]';
+  } else if (Array.isArray(obj)) {
+    var values = '';
+    for (var i = 0; i < obj.length; i++) {
+      if (i === obj.length - 1) {
+        values += stringifyJSON(obj[i]);
+        continue;
+      }
+      values += stringifyJSON(obj[i]) + ',';
+    }
+    return '[' + values + ']';
+  } else if (typeof obj === 'object') {
+    var properties = '';
+    for (var key in obj) {
+      if (key === 'functions') {
+        continue;
+      }
+      if (key === 'undefined') {
+        continue;
+      }
+      properties += '"' + key + '"' + ':' + stringifyJSON(obj[key]) + ',';
+    }
+    properties = properties.slice(0, properties.length - 1);
+    return '{' + properties + '}';
+  }
 };
