33dcae352a294cfea759c926f75028c1c3336a79
Phuc Tran <phuctranlv@gmail.com>
2020-01-25 21:13:47 -0800

    Complete getElementsByClassName function definition

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,43 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className) {
+    // your code here
+
+    var currentNode = this;
+    if (this.document) {
+      var currentNode = document.body;
+    }
+
+    var children = currentNode.childNodes;
+
+    if (currentNode.classList) {
+      if (currentNode.classList.value.includes(className)) {
+        if (this.document && children) {
+          var theFirstOneHasIt = currentNode;
+        } else if (!this.document && children.length === 0) {
+          return currentNode;
+        } else if (!this.document) {
+          var hasChildrenButAlsoHasClass = currentNode;
+        }
+      }
+    }
+
+    if (children) {
+      var outputArray = [];
+      if (theFirstOneHasIt) {
+        outputArray.push(theFirstOneHasIt);
+      }
+      if (hasChildrenButAlsoHasClass) {
+        outputArray.push(hasChildrenButAlsoHasClass);
+      }
+      for (var i = 0; i < children.length; i++) {
+        var result = getElementsByClassName.call(children[i], className);
+        if (result !== undefined) {
+          outputArray.push(result);
+        }
+      }
+      return outputArray.flat(Infinity)
+    }
+
+  };
